749c1f701320dd399ee6e710aeb631f027fc0ffc
Khadimul Islam <32012055-khadimul723co@users.noreply.replit.com>
2024-08-25 15:43:35 +0000

    Day -1 printing statement

---
+++ main.py
@@ -0,0 +1,24 @@
+# stirng print
+print('Hello Python')
+
+# int print
+print(123)
+
+# sum int print
+print(1 + 2)
+
+# odd print
+print(2 - 1)
+
+# print statement task: print this line
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+print('1. "Mix 500g of Flour", 10g Yeast and 300ml Water in a bowl.')
+print('2. Knead the dough for 10 minutes.')
+print('2. Knead the dough for 10 minutes.')
+print('3. Add 3g of Salt.')
+print("4. Leave to rise for \"2 hours.\"")
