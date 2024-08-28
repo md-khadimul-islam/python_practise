e4dafb6df889c756b456a8cfcdc2f6113b28dc22
Md.Khadimul Islam <150368814+md-khadimul-islam@users.noreply.github.com>
2024-08-28 22:00:37 +0600

    Day 01 practice

---
+++ Day 01/day_1_final_project.py
@@ -0,0 +1,4 @@
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+band_name = 'Your band name could be '
+print(band_name + city_name + " " + pet_name)
---
+++ Day 01/input_function.py
@@ -0,0 +1,22 @@
+print("Hello " + "" + input("what is your name?"))
+
+input("What is your pet name")
+
+# Charecter count
+charecter = "This is my name."
+
+print(len(charecter))
+
+# Exercise -1
+a = 29
+b = 41
+
+# python always (= sign) previous letrer store in current value
+c = a
+a = b
+b = c
+
+print("a: " + str(a))
+print("b: " + str(b))
+
+# passed_exam = True
