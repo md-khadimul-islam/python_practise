67de2b1be75798dfcfb3d12d1524fd82401f895c
Md.Khadimul Islam <150368814+md-khadimul-islam@users.noreply.github.com>
2024-08-30 19:27:52 +0600

    data types

---
+++ Day 02/data_types.py
@@ -0,0 +1,24 @@
+# -----------DataTypes----------
+
+
+# String
+
+test = "Help"
+print(test[3])
+
+
+# Integer
+
+num = 253
+print(num)
+
+
+# float
+
+num = 3.1416
+print(num)
+
+
+# bool
+True
+False
