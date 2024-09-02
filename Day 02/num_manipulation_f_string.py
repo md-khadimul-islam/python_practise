34d53e7069a431244ce1eea0ffb371db610a97d5
Md.Khadimul Islam <150368814+md-khadimul-islam@users.noreply.github.com>
2024-09-02 22:44:31 +0600

    weeks calculate

--- Day 02/num_manipulation_f_string.py
+++ Day 02/num_manipulation_f_string.py
@@ -33,3 +33,30 @@ score = 50
 height = 5.6
 
 print(f"Your score is: {score}\nYour height is: {height}")
+
+
+# -----------Quiz 03-------------
+# I was reading this article by Tim Urban - Your Life in Weeks and realised just how little time we actually have.
+
+# Create a program using maths and f-Strings that tells us how many weeks we have left, if we live until 90 years old.
+
+# It will take your current age as the input and output a message with our time left in this format:
+
+# You have x weeks left.
+
+# Where x is replaced with the actual calculated number of weeks the input age has left until age 90.
+
+# Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+
+per_years = 52
+
+age = int(input("Please enter your age "))
+
+alive_age = 90
+
+actual_age = 90 - age
+
+left_weeks = f"You have {actual_age * per_years} weeks left"
+
+print(left_weeks)
