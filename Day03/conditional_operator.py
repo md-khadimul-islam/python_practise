d63422adc00c49e6f3ea0870ba45f77ca3365b9c
Md.Khadimul Islam <150368814+md-khadimul-islam@users.noreply.github.com>
2024-09-09 21:29:21 +0600

    conditional operators

---
+++ Day03/conditional_operator.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What's your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+
+# -------------- Note ------------
+# == Equal to
+# > Getter than
+# < less than
+# <= less than equal
+# >= getter than equal
